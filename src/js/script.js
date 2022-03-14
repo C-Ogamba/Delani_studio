9b57b31258415049461bd377f100328f7fbe5320
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-03-14 15:35:46 +0300

    fix: make the alert section functional

--- src/js/script.js
+++ src/js/script.js
@@ -28,4 +28,25 @@ const designForm = () => {
        $("h6").css("font-weight", "bold");
      });
 
+};
+function alertMessage() {
+    let pop = document.querySelector("#pop");
+    let empty = document.querySelector("#name").value && document.querySelector("#email").value;
+        if (empty == ""){
+            pop.classList.toggle("none");
+            alert("Fill in all the spaces");
+            return false;
+
+        }else {
+            pop.classList.toggle("show");
+            document.querySelector("#pop").innerHTML =
+            document.querySelector("#name").value + ", your message has been recorded."
+         };
 }
+// document
+//   .getElementById("btn")
+//   .addEventListener("click", function (event) {
+//     event.preventDefault();
+//   });
+
+$("input[type='submit']").click(function subFunc(){return false});
