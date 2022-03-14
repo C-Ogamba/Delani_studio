f74f0185774442968121bc81d2215d9bdb324e1c
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-03-14 09:18:16 +0300

    make the elements toggle on click

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,18 @@
 "use strict";
 $(document).ready(function () {
-    
+    $("#design").click(function (){
+        $(".design1").toggle();
+        $("#design2").fadeToggle(400, "linear");
+        $("h6").css("font-weight", "bold")
+    })
+     $("#dev").click(function () {
+       $(".dev2").toggle();
+       $("#dev1").fadeToggle(400, "linear");
+     });
+     $("#pro").click(function () {
+       $(".pro2").toggle();
+       $("#pro1").fadeToggle(400, "linear");
+     });
+
+
 })
