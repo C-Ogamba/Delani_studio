7cfab599b1805984f99935a7a33415f3dfb5fd33
Kemunto Cindy <cindy.kemu@student.moringaschool.com>
2022-03-15 09:20:28 +0300

    fix:change the transition of the portfolio elements

--- src/js/script.js
+++ src/js/script.js
@@ -3,11 +3,11 @@ $(document).ready(function () {
    designForm();
      $("#dev").click(function () {
        $(".dev2").toggle();
-       $("#dev1").fadeToggle(400, "linear");
+       $("#dev1").fadeToggle(200, "linear");
      });
      $("#pro").click(function () {
        $(".pro2").toggle();
-       $("#pro1").fadeToggle(400, "linear");
+       $("#pro1").fadeToggle(200, "linear");
      });
 
 
@@ -24,7 +24,7 @@ $(document).ready(function () {
 const designForm = () => {
      $("#design").click(function () {
        $(".design1").toggle();
-       $("#design2").fadeToggle(400, "linear");
+       $("#design2").fadeToggle(200, "linear");
        $("h6").css("font-weight", "bold");
      });
 
